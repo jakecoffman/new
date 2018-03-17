5b249cf0b9092b85f2fffaca1916d517a44f821a
jake <jake@jakecoffman.com>
2018-03-17 15:19:18 -0500

    make it mine

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,226 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        trynuxt
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">SUp</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <section class="theme-default">
+    <div class="wrapper">
+      <header class="page-header">
+        <h1 class="header-name" itemprop="name">Jake Coffman</h1>
+        <div class="header-contact-info">
+          <p>jake@jakecoffman.com</p>
+        </div>
+        <div class="title-bar no-print">
+          <h2 class="header-title" itemprop="jobTitle">Software Artisan</h2>
+          <ul class="icon-links">
+
+            <!-- GitHub link -->
+            <li class="icon-link-item">
+              <a href="https://github.com/jakecoffman" class="icon-link" itemprop="sameAs">
+                <svg class="icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve" width="28">
+<path id="GitHub" fill-rule="evenodd" clip-rule="evenodd" fill="#D1CECC" d="M14.01,0C6.27,0-0.01,6.28-0.01,14.02
+  c0,6.19,4.02,11.45,9.59,13.3c0.7,0.13,0.96-0.3,0.96-0.68c0-0.33-0.01-1.21-0.02-2.38c-3.9,0.85-4.72-1.88-4.72-1.88
+  c-0.64-1.62-1.56-2.05-1.56-2.05c-1.27-0.87,0.1-0.85,0.1-0.85c1.41,0.1,2.15,1.44,2.15,1.44c1.25,2.14,3.28,1.52,4.08,1.16
+  c0.13-0.91,0.49-1.52,0.89-1.87c-3.11-0.35-6.38-1.56-6.38-6.93c0-1.53,0.55-2.78,1.44-3.76C6.37,9.17,5.89,7.74,6.65,5.81
+  c0,0,1.18-0.38,3.85,1.44c1.12-0.31,2.32-0.47,3.51-0.47c1.19,0.01,2.39,0.16,3.51,0.47c2.68-1.81,3.85-1.44,3.85-1.44
+  c0.76,1.93,0.28,3.35,0.14,3.71c0.9,0.98,1.44,2.23,1.44,3.76c0,5.38-3.28,6.57-6.4,6.92c0.5,0.43,0.95,1.29,0.95,2.6
+  c0,1.87-0.02,3.39-0.02,3.84c0,0.37,0.25,0.81,0.96,0.67c5.56-1.86,9.58-7.11,9.58-13.3C28.03,6.28,21.75,0,14.01,0z"></path>
+</svg>
+              </a>
+            </li>
+
+            <!-- Twitter link -->
+            <li class="icon-link-item"><a href="http://twitter.com/nill" class="icon-link" itemprop="sameAs">
+              <svg class="icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve" width="28">
+<path id="Twitter" fill="#D1CECC" d="M14,0C6.27,0,0,6.27,0,14s6.27,14,14,14s14-6.27,14-14S21.73,0,14,0z M20.69,10.57
+  c0.01,0.15,0.01,0.3,0.01,0.45c0,4.56-3.47,9.82-9.82,9.82c-1.95,0-3.76-0.57-5.29-1.55c0.27,0.03,0.54,0.05,0.82,0.05
+  c1.62,0,3.11-0.55,4.29-1.48c-1.51-0.03-2.79-1.03-3.23-2.4c0.21,0.04,0.43,0.06,0.65,0.06c0.31,0,0.62-0.04,0.91-0.12
+  c-1.58-0.32-2.77-1.71-2.77-3.39c0-0.01,0-0.03,0-0.04c0.47,0.26,1,0.41,1.56,0.43c-0.93-0.62-1.54-1.68-1.54-2.87
+  c0-0.63,0.17-1.23,0.47-1.74c1.7,2.09,4.25,3.46,7.12,3.61c-0.06-0.25-0.09-0.52-0.09-0.79c0-1.91,1.55-3.45,3.45-3.45
+  c0.99,0,1.89,0.42,2.52,1.09c0.79-0.15,1.53-0.44,2.19-0.84c-0.26,0.81-0.81,1.48-1.52,1.91c0.7-0.08,1.36-0.27,1.98-0.54
+  C21.95,9.47,21.37,10.08,20.69,10.57z"></path>
+</svg>
+            </a></li>
+
+            <!-- Website link -->
+            <li class="icon-link-item"><a href="https://jakecoffman.com" class="icon-link" itemprop="url">
+              <svg class="icon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 28 28" enable-background="new 0 0 28 28" xml:space="preserve" width="28">
+<path id="Website" fill="#D1CECC" d="M14.83,7.6c0.03,0.08,0.03,0.14,0,0.19s-0.08,0.07-0.17,0.07c-0.12,0-0.26-0.01-0.4-0.04
+  s-0.2-0.06-0.18-0.1c0.15-0.15,0.16-0.26,0.03-0.33c-0.13-0.07-0.27-0.11-0.42-0.11s-0.25-0.04-0.3-0.09s-0.03-0.09,0.05-0.09
+  s0.22,0,0.4,0.01c0.19,0.01,0.31,0.04,0.37,0.08s0.17,0.1,0.33,0.19S14.8,7.52,14.83,7.6z M15.9,13.35c-0.11-0.03-0.2-0.03-0.26,0
+  s-0.13,0.05-0.2,0.05c-0.08-0.01-0.25-0.09-0.5-0.24s-0.5-0.22-0.77-0.22c-0.11,0.01-0.17,0.04-0.19,0.08s-0.01,0.07,0.01,0.08
+  c0.02,0.01,0.12,0.03,0.29,0.05s0.4,0.11,0.67,0.26c0.28,0.15,0.48,0.21,0.61,0.19s0.22-0.06,0.28-0.1
+  c0.07-0.01,0.12-0.03,0.16-0.06S16.01,13.38,15.9,13.35z M13.62,6.85c0.17-0.03,0.3-0.13,0.38-0.3c0.03-0.14-0.04-0.2-0.21-0.18
+  s-0.29,0.07-0.37,0.13c-0.01,0.03-0.02,0.06-0.03,0.09s-0.02,0.06-0.03,0.07l-0.08,0.1c-0.03,0.03-0.03,0.05-0.02,0.06
+  C13.32,6.88,13.44,6.89,13.62,6.85z M16.12,7.08c-0.02-0.16-0.11-0.3-0.26-0.41s-0.32-0.19-0.5-0.22c-0.18-0.03-0.38-0.07-0.61-0.12
+  s-0.4-0.04-0.53,0.04c-0.21,0.18-0.25,0.31-0.12,0.39c0.07,0.03,0.2,0.05,0.38,0.06s0.44,0.15,0.76,0.41
+  c0.11,0.08,0.29,0.21,0.55,0.37s0.44,0.23,0.55,0.19c0.06-0.06,0.04-0.15-0.05-0.28S16.14,7.24,16.12,7.08z M28,14
+  c0,7.73-6.27,14-14,14S0,21.73,0,14S6.27,0,14,0S28,6.27,28,14z M21.95,14c-0.06-2.25-0.83-4.12-2.33-5.62
+  c-1.5-1.5-3.37-2.27-5.62-2.33c-2.25,0.06-4.12,0.83-5.62,2.33c-1.5,1.5-2.27,3.37-2.33,5.62c0.06,2.25,0.83,4.12,2.33,5.62
+  c1.5,1.5,3.37,2.27,5.62,2.33c0.54,0,1.06-0.05,1.57-0.14c-0.11-0.07-0.17-0.24-0.18-0.51s0.04-0.52,0.16-0.76
+  c0.15-0.26,0.3-0.61,0.46-1.04c0.15-0.43,0.08-0.77-0.23-1c-0.32-0.18-0.51-0.34-0.57-0.49s-0.19-0.32-0.37-0.54
+  s-0.28-0.36-0.28-0.44v-0.21c-0.01-0.21,0.05-0.43,0.19-0.66l0.25-0.39c0.01-0.04,0.02-0.1,0.02-0.19v-0.19
+  c-0.01-0.04-0.07-0.09-0.17-0.16s-0.17-0.1-0.22-0.1s-0.09,0.01-0.11,0.05s-0.07,0.06-0.13,0.06s-0.17-0.04-0.33-0.13
+  c-0.16-0.09-0.27-0.16-0.32-0.21s-0.1-0.11-0.14-0.19s-0.09-0.15-0.16-0.22s-0.11-0.1-0.16-0.09s-0.12,0-0.23-0.03l-1.45-0.56
+  c-0.26-0.11-0.43-0.23-0.5-0.37c-0.07-0.14-0.1-0.26-0.1-0.37s-0.06-0.23-0.17-0.37l-0.29-0.37c-0.08-0.12-0.14-0.24-0.17-0.34
+  s-0.05-0.15-0.06-0.13c-0.01,0.07,0.02,0.23,0.1,0.48s0.12,0.39,0.1,0.43c-0.03,0.01-0.07-0.02-0.12-0.11s-0.12-0.19-0.19-0.3
+  c-0.06-0.07-0.07-0.15-0.04-0.25c0.03-0.1-0.07-0.38-0.29-0.85c-0.22-0.47-0.26-0.85-0.11-1.15s0.24-0.57,0.3-0.82
+  c0.03-0.21,0.09-0.25,0.19-0.13c0.1,0.12,0.1,0.11,0.02-0.01c-0.08-0.18-0.11-0.4-0.08-0.65c0.03-0.26,0.01-0.46-0.06-0.61
+  c-0.11-0.1-0.3-0.08-0.58,0.05S9.12,8.14,9.1,8.16c0.03-0.12,0.18-0.28,0.46-0.46l0.87-0.58c0.26-0.17,0.49-0.22,0.69-0.17
+  s0.41,0.12,0.62,0.21s0.34,0.12,0.37,0.1s0.01-0.06-0.06-0.13s-0.07-0.13,0.01-0.16S12.26,6.96,12.43,7
+  c0.17,0.03,0.31,0.11,0.42,0.24s0.3,0.18,0.54,0.17s0.32,0.01,0.25,0.06s-0.11,0.12-0.11,0.19c0.04,0.06,0.02,0.1-0.05,0.13
+  s-0.2,0.1-0.36,0.21s-0.18,0.19-0.05,0.26c0.13,0.07,0.36,0.21,0.67,0.41c0.28,0.19,0.41,0.19,0.41-0.01s-0.02-0.4-0.06-0.61
+  c-0.01-0.14,0.05-0.19,0.2-0.14s0.22,0.07,0.24,0.08c0.17,0.11,0.27,0.15,0.32,0.11s0.13-0.04,0.26-0.01
+  c0.17,0.07,0.37,0.19,0.6,0.37l0.41,0.33c-0.43,0.25-0.6,0.41-0.51,0.49s0.2,0.14,0.32,0.2c0.06,0.06,0.02,0.12-0.1,0.21
+  s-0.2,0.12-0.23,0.12c-0.08-0.08-0.15-0.1-0.2-0.05s-0.1,0.09-0.16,0.13c-0.06,0.04-0.07,0.09-0.05,0.14s0.04,0.09,0.05,0.1
+  c-0.3,0.07-0.47,0.22-0.49,0.44s-0.02,0.39-0.01,0.49c0,0.08-0.07,0.18-0.21,0.3s-0.24,0.23-0.31,0.34c-0.03,0.14,0,0.29,0.08,0.47
+  c0.08,0.17,0.08,0.27,0,0.3c-0.08-0.01-0.22-0.12-0.41-0.32c-0.19-0.2-0.55-0.24-1.08-0.13c-0.19,0.04-0.39,0.16-0.6,0.36
+  s-0.23,0.49-0.06,0.88c0.21,0.32,0.43,0.38,0.68,0.19c0.25-0.19,0.43-0.3,0.56-0.31c0.1,0.08,0.12,0.2,0.06,0.35
+  c-0.06,0.15-0.08,0.24-0.08,0.27c0.06-0.01,0.17,0.02,0.34,0.09s0.28,0.27,0.32,0.58c0.04,0.31,0.19,0.49,0.46,0.55
+  s0.45,0.08,0.57,0.07s0.28-0.09,0.48-0.24s0.32-0.24,0.37-0.27s0.17-0.06,0.35-0.1s0.47,0.03,0.84,0.23s0.67,0.31,0.88,0.35
+  s0.38,0.11,0.5,0.2s0.18,0.2,0.18,0.33s0.05,0.23,0.14,0.3c0.15,0.04,0.39,0.07,0.72,0.07s0.57,0.1,0.72,0.28
+  c0.07,0.28-0.08,0.73-0.46,1.38c-0.37,0.64-0.68,1.05-0.93,1.23c-0.18,0.12-0.34,0.29-0.48,0.49s-0.3,0.38-0.49,0.55
+  s-0.41,0.34-0.66,0.52s-0.51,0.34-0.78,0.5c-0.21,0.15-0.36,0.32-0.46,0.51s-0.19,0.32-0.27,0.4c1.81-0.43,3.28-1.34,4.43-2.75
+  C21.31,17.6,21.9,15.93,21.95,14z"></path>
+</svg>
+            </a></li>
+          </ul>
+
+        </div>
+        <div class="executive-summary" itemprop="description">
+          <p>
+            I am a professional software developer of 11+ years. I am good at many things but
+            especially experienced in web, real-time (games), networking, and writing maintainable code. I have
+            professional experience with C++, Python, Java, Ruby, JavaScript, Groovy, C#, and Go. In my
+            free time I do Go web apps, games in Unity, and I developed an Android app using gRPC.
+          </p>
+          <br/>
+          <p>
+            I have quite a few open source projects, be sure to go see my Github account!
+          </p>
+        </div>
+
+      </header>
+      <!--Experience-->
+      <section class="content-section">
+
+        <header class="section-header">
+          <h2>Experience</h2>
+        </header>
+
+        <div class="resume-item" itemscope="" itemprop="worksFor" itemtype="http://schema.org/Organization">
+          <h3 class="resume-item-title" itemprop="name">World Wide Technology</h3>
+          <h4 class="resume-item-details" itemprop="description">Senior Software Developer • 2015 — Present</h4>
+          <p class="resume-item-copy">
+            I work on various web applications and micro-services doing full stack development.
+          <p class="resume-item-copy">
+            <b>Technologies:</b> JavaScript (VueJS, NodeJS, AngularJS), Groovy on Grails, Go, HTML/CSS
+          </p>
+        </div><!-- end of resume-item -->
+
+        <div class="resume-item" itemscope="" itemprop="worksFor" itemtype="http://schema.org/Organization">
+          <h3 class="resume-item-title" itemprop="name">Appistry</h3>
+          <h4 class="resume-item-details" itemprop="description">Software Developer • 2012 — 2015</h4>
+          <p class="resume-item-copy">
+            I was hired to work on the proprietary Appistry cloud but quickly switched to working on supporting apps
+            that were web based. Also developed a Data Warehouse for the University of Maryland Medical Center to make
+            patient data searchable.
+          </p>
+          <p class="resume-item-copy">
+            <b>Technologies:</b> C++, Java (Jersey), HTML/CSS, Python (Django/Mezzanine), Groovy on Grails
+          </p>
+        </div><!-- end of resume-item -->
+
+        <div class="resume-item" itemscope="" itemprop="worksFor" itemtype="http://schema.org/Organization">
+          <h3 class="resume-item-title" itemprop="name">Boeing</h3>
+          <h4 class="resume-item-details" itemprop="description">Software Developer/Technical Lead • 2009 — 2012</h4>
+          <p class="resume-item-copy">
+            For the first year I worked on an instructor station for a flight training system. I did networking and GUI
+            design and implementation. After that I moved into a Tech Lead position where I had a team that worked on
+            development tools.
+          </p>
+          <p class="resume-item-copy">
+            <b>Technologies:</b> C++, Python (Flask)
+          </p>
+        </div><!-- end of resume-item -->
+
+        <div class="resume-item" itemscope="" itemprop="worksFor" itemtype="http://schema.org/Organization">
+          <h3 class="resume-item-title" itemprop="name">Cerner</h3>
+          <h4 class="resume-item-details" itemprop="description">Software Developer • 2006 — 2009</h4>
+          <p class="resume-item-copy">I worked on C libraries around security and various security related GUIs.</p>
+          <p class="resume-item-copy">
+            <b>Technologies:</b> C, VB6
+          </p>
+        </div><!-- end of resume-item -->
+
+
+        <!--Education-->
+      </section>
+
+      <section class="content-section">
+        <header class="section-header">
+          <h2>Education</h2>
+        </header>
+
+        <div class="resume-item" itemscope="" itemprop="alumniOf" itemtype="http://schema.org/CollegeOrUniversity">
+          <h3 class="resume-item-title" itemprop="name">University of Missouri at Rolla</h3>
+          <h4 class="resume-item-details group" itemprop="description">Bachelors Degree, Computer Science • 2002 —
+            2006</h4>
+          <h5 class="resume-item-details award-title" itemprop="description"></h5>
+          <p class="resume-item-copy" itemprop="description">
+          </p>
+          <ul class="resume-item-list">
+            <li>Cum Laude</li>
+          </ul>
+        </div>
+
+      </section>
+
+      <!-- Projects -->
+      <!--<section class="content-section">-->
+        <!--<header class="section-header">-->
+          <!--<h2>Projects</h2>-->
+        <!--</header>-->
+
+        <!--<div class="resume-item" itemscope="" itemtype="http://schema.org/CreativeWork">-->
+          <!--<meta itemprop="creator" content="Homer J. Simpson" itemtype="http://schema.org/Person">-->
+          <!--<h3 class="resume-item-title" itemprop="name">donutfinder.io</h3>-->
+          <!--<h4 class="resume-item-details" itemprop="description">Founder &amp; Primary Developer • 2012 — Present</h4>-->
+          <!--<p class="resume-item-copy">Donut Locator is an open source node/ember app that lets users find donuts within-->
+            <!--a defined radius from their home.</p>-->
+        <!--</div>-->
+
+        <!--<div class="resume-item" itemscope="" itemtype="http://schema.org/CreativeWork">-->
+          <!--<meta itemprop="creator" content="Homer J. Simpson" itemtype="http://schema.org/Person">-->
+          <!--<h3 class="resume-item-title" itemprop="name">Springfield Donut Eater's User Group</h3>-->
+          <!--<h4 class="resume-item-details" itemprop="description">Co-Founder &amp; Organizer • 2007 — Present</h4>-->
+          <!--<p class="resume-item-copy">DEUG is a local monthly meetup in Springfield where we share all the latest tips-->
+            <!--and tricks for dat donut lifestyle. I organize the group, and typically eat most of the donuts.</p>-->
+        <!--</div>-->
+
+
+      <!--</section>-->
+
+      <footer class="page-footer">
+        <p class="footer-line">Thanks for reading!</p>
+      </footer>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+  import AppLogo from '~/components/AppLogo.vue'
 
-export default {
-  components: {
-    AppLogo
+  export default {
+    components: {
+      AppLogo
+    }
   }
-}
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="scss">
+  @import "~assets/_sass/normalize";
+  @import "~assets/_sass/mixins";
+  @import "~assets/_sass/variables";
+  // Generic site base & layout styles
+  @import "~assets/_sass/base";
+  @import "~assets/_sass/layout";
+  // Layouts
+  @import "~assets/_sass/resume";
 </style>
