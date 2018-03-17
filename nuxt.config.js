1f3a54337081cdefd53caef7a4ced646c02a2123
jake <jake@jakecoffman.com>
2018-03-17 15:29:07 -0500

    fix paths

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,12 +1,8 @@
-// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
-const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
-  router: {
-    base: '/new/'
-  }
-} : {};
 
 module.exports = {
-  routerBase,
+  router: {
+    base: '/new/'
+  },
   /*
   ** Headers of the page
   */
